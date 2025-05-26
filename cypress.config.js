928dd7510b35982e59c9a7eefef236ef1461d81a
ismiazis96 <mohamadismiazis@gmail.com>
2025-05-26 12:16:07 +0700

    push project final progress

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+// setup env
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  screenshotOnRunFailure: true,
+  video: true, // fungsi buat rekam saat running
+  videoCompression: 32,
+  watchForFileChanges: false, // fungsi buat otomatis jalan saat di save
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
+  e2e: {
+    defaultCommandTimeout: 10000,
+    env: {
+      ...process.env
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
+    },
+  },
+});
