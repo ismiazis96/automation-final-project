16f031caea50482358b795361b6cd08ab25363cc
ismiazis96 <mohamadismiazis@gmail.com>
2025-06-07 20:36:34 +0700

    update pom with element fix

--- cypress/support/page-object/agoda/booking.js
+++ cypress/support/page-object/agoda/booking.js
@@ -14,7 +14,7 @@ class orderTicket{
         cy.xpath(`${element.booking.selectFrom}`).click();
         cy.xpath(`${element.booking.to}`).type(tujuanAkhir);
         cy.xpath(`${element.booking.selectTo}`).click();
-        cy.xpath(`${element.booking.date}`).click();
+        cy.xpath(`${element.booking.dateWeekend}`).click();
         cy.xpath(`${element.booking.buttonSearch}`).click();
 
         // wrap value untuk di pake lain tempat
@@ -46,12 +46,12 @@ class orderTicket{
     }
 
     contactInformation({
-        firsname=Cypress.env('contactFirstName'),
+        firstname=Cypress.env('contactFirstName'),
         lastname=Cypress.env('contactLastName'),
         email=Cypress.env('contactEmail'),
         phoneNumber=Cypress.env('contactPhoneNumber')
     }) {
-        cy.xpath(`${element.contact.firstname}`).type(firsname);
+        cy.xpath(`${element.contact.firstname}`).type(firstname);
         cy.xpath(`${element.contact.lastname}`).type(lastname);
         cy.xpath(`${element.contact.email}`).type(email);
         cy.xpath(`${element.contact.phone}`).type(phoneNumber);
@@ -69,14 +69,14 @@ class orderTicket{
         
         cy.then(() => {
             if(passengerGender === 'Pria'){
-                cy.xpath(`//div[@data-testid="flight.forms.i0.units.i0.passengerGender"]//input[@aria-label="Pria" and @type="radio"]`).click();
-                cy.xpath(`//div[@data-testid="flight.forms.i0.units.i0.passengerGender"]//input[@aria-label="Pria" and @type="radio"]`).click();
+                cy.xpath(`${element.passenger.genderPria}`).click();
+                cy.xpath(`${element.passenger.genderPria}`).click();
             }else{
-                cy.xpath(`//div[@data-testid="flight.forms.i0.units.i0.passengerGender"]//input[@aria-label="Wanita" and @type="radio"]`).check();
+                cy.xpath(`${element.passenger.genderWanita}`).check();
             }
         });
         
-        cy.xpath(`${element.passenger.firsname}`).type(passengerFirstname);
+        cy.xpath(`${element.passenger.firstname}`).type(passengerFirstname);
         cy.xpath(`${element.passenger.lastname}`).type(passengerLastname);
 
         // wrap as full name
@@ -90,9 +90,7 @@ class orderTicket{
         cy.xpath(`${element.passenger.years}`).type(PassengerYears);
         cy.xpath(`${element.passenger.btnNationality}`).click();
         cy.xpath(`${element.passenger.typeNationality}`).type(passengerNationality);
-        cy.xpath(`${element.passenger.selectNationality}`).click();
-
-        
+        cy.xpath(`${element.passenger.selectNationality}`).click();    
 
     }
 
@@ -104,37 +102,37 @@ class orderTicket{
         PassportYears=Cypress.env('PASSPORT_YEARS')
     }) {
  
-        cy.xpath(`//input[@id="flight.forms.i0.units.i0.passportNumber"]`).click().type(passportNumber);
-        cy.xpath(`//div[@data-testid="flight.forms.i0.units.i0.passportCountryOfIssue"]//button`).click();
-        cy.xpath(`//input[@placeholder="Cari"]`).type(passportCountry);
-        cy.xpath(`//ul[@role='listbox']`).click();
-        cy.xpath(`//input[@data-testid='flight.forms.i0.units.i0.passportExpiryDate-DateInputDataTestId']`).type(passportDate);
-        cy.xpath(`//div[@data-testid="flight.forms.i0.units.i0.passportExpiryDate-MonthInputDataTestId"]`).click();
-        cy.xpath(`//div[@data-testid="floater-container"]//span[contains(text(),'${passportMonth}')]`).click();
-        cy.xpath(`//input[@datatestid="flight.forms.i0.units.i0.passportExpiryDate-YearInputDataTestId"]`).type(PassportYears);
+        cy.xpath(`${element.passport.passportNumber}`).click().type(passportNumber);
+        cy.xpath(`${element.passport.passportCountry}`).click();
+        cy.xpath(`${element.passport.typePassportCountry}`).type(passportCountry);
+        cy.xpath(`${element.passport.selectPassportCountry}`).click();
+        cy.xpath(`${element.passport.passportDate}`).type(passportDate);
+        cy.xpath(`${element.passport.btnPassportMonth}`).click();
+        cy.xpath(`//div[@data-testid='floater-container']//span[contains(text(),'${passportMonth}')]`).click();
+        cy.xpath(`${element.passport.passportYears}`).type(PassportYears);
 
     }
 
     addOn() {
-        cy.xpath(`//button[@data-component="flight-continue-to-addOns-button"]`).click({ force: true });
-        cy.xpath('//div[@role="radiogroup"]//div[@data-testid="radio-button-option-no"]').click({ multiple: true, force: true });
+        cy.xpath(`${element.addOn.continueToAddOnsButton}`).click({ force: true });
+        cy.xpath(`${element.addOn.noAddOns}`).click({ multiple: true, force: true });
         // cy.wait(5000);
-        cy.xpath(`//button[@data-testid="continue-to-payment-button"]`).click();
+        cy.xpath(`${element.addOn.continueToPaymentButton}`).click();
     }
 
     pagePaymentAssertion() {
         cy.get("@fullName").then((fullName) => {
-            cy.xpath(`//div[@data-component='passenger-summary-list']//strong[@data-component="name-container-name"]`).invoke("text").should("contain", fullName);
+            cy.xpath(`${element.assertPayment.fullName}`).invoke("text").should("contain", fullName);
         });
 
         cy.get("@tujuanAwal").then((tujuanAwal) => {
-            cy.xpath(`//div[@data-component='flight-booking-itineraryHeader']//h5[1]`).invoke("text").should("contain", tujuanAwal);
+            cy.xpath(`${element.assertPayment.tujuanAwal}`).invoke("text").should("contain", tujuanAwal);
         });
 
         cy.get("@tujuanAkhir").then((tujuanAkhir) => {
-            cy.xpath(`//div[@data-component='flight-booking-itineraryHeader']//h5[2]`).invoke("text").should("contain", tujuanAkhir);
+            cy.xpath(`${element.assertPayment.tujuanAkhir}`).invoke("text").should("contain", tujuanAkhir);
         });
-        cy.xpath(`//dd[@data-component='mob-flight-price-total-desc']//span`).should('be.visible');
+        cy.xpath(`${element.assertPayment.price}`).should('be.visible');
     }
 
 }
